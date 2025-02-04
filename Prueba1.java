7dee50e7cf6d48a8ea34d42f642162a29945ed52
Jhonatan-2007 <jesguerra@estudiante.uniajc.edu.co>
2025-02-04 17:20:52 +0000

    Suma Extremo Arreglos

--- Prueba1.java
+++ Prueba1.java
@@ -1,7 +1,9 @@
 public class Prueba1 {
     public static void main(String[] args) {
-        int[] a = {2, 5, 8, 3, 1, 10, 11};
+        int[] a = {4, 8, 2, 1, 6, 7};
         System.out.println(sumaParesImparesArreglo(a));
+        System.out.println(sumaExtremosArreglos(a));
+        int [] b = {};
     }
 
     public static String sumaParesImparesArreglo(int[] a) {
@@ -17,5 +19,22 @@ public class Prueba1 {
         cad = "la suma de los numeros pares = "+ sumapares +" suma de los numeros impares = "+sumaimpares;
         return cad;
     }
+
+    public static String sumaExtremosArreglos(int[] a){
+        String cad = "";
+            //Verificar si el arreghlos es par o impar 
+            if(a.length%2 == 0){
+                //el arreglo es par
+                for(int i = 0; i< a.length/2; i++){
+                    cad+=(a[i]+ (a[a.length-1-i]))+ " ";
+                }
+            }else{
+                // el arrglo es impar
+                cad+="arreglo impar";
+            }
+
+        return cad;
+    }
 }
+
 
