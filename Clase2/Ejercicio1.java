c43c09504419f96a0681a1608624202532ebf11c
Jhonatan-2007 <jesguerra@estudiante.uniajc.edu.co>
2025-02-11 17:12:58 +0000

    SumaYPromedioDeArreglos

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -7,7 +7,8 @@ public class Ejercicio1 {
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println(op.mostrarElementos(a));
-
+        System.out.println("La suma de los elementos del arreglo es: "+op.sumarElementos(a));
+        System.out.println("El promedio de los elementosdel arreglo es: "+op.promedioElementos(a));
         
     }
 }
