208692d50e14d7466a50d2903c00a3981250a344
Jhonatan-2007 <jesguerra@estudiante.uniajc.edu.co>
2025-02-14 00:31:47 +0000

    Ejercicio 1 del taller

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         OperacionesConArreglos op = new OperacionesConArreglos();
 
         /* 
@@ -11,9 +12,25 @@ public class Ejercicio1 {
         System.out.println("La suma de los elementos del arreglo es: "+op.sumarElementos(a));
         System.out.println("El promedio de los elementosdel arreglo es: "+op.promedioElementos(a));
         */
-        
+
+        /* 
         int n = 5;
         int[] a = op.llenarArregloAleatorios(n);
         System.out.println(op.mostrarElementos(a));
+        */
+
+        /*Ejercicio 1 
+        System.out.println("Ingrese el tamaño del arreglo (n): ");
+        int n = scanner.nextInt();
+        int[] a = op.llenarArregloAleatorios(n);
+        System.out.println(op.mostrarElementos(a));
+        */
+
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Ejercicio1 []";
     }
 }
