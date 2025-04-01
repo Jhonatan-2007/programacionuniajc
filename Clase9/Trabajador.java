faac53423f7dbe18dad18a7b5e959d490c09af89
Jhonatan-2007 <jesguerra@estudiante.uniajc.edu.co>
2025-04-01 16:07:36 +0000

    Creacion de la carpeta clase 9

---
+++ Clase9/Trabajador.java
@@ -0,0 +1,16 @@
+public class Trabajador {
+
+        public String Cedula;
+        public String Nombre;
+        public double Salario;
+    
+        public Trabajador(String Cedula, String Nombre, double Salario){
+            this.Cedula = cedula;
+            this.Nombre = nombre;
+            this.Salario = salario;
+        }
+
+        public String pagar(){
+            return "";
+        }
+}
